c08bb4d45fcfc684d8587a8488a5576f59e6099e
dream10201 <201269807@qq.com>
2019-07-29 22:30:33 +0800

    Initial Commit

---
+++ MiniTranslation-Mac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MiniTranslation-Mac
+//
+//  Created by dream on 2019/7/29.
+//  Copyright © 2019 dream. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
